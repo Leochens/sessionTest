77b45aa92e7f7a72c91196489324d294d1ea0f87
Leochens·ZHL <18332518328@163.com>
2018-10-04 11:49:10 +0800

    连接数据库suc

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$CONFIG = array(
+  'host'=>'127.0.0.1',
+  'user'=>'root',
+  'passwd'=>'123456',
+  'dbname'=>'todolist',
+  'port'=>'3306'
+);
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+// 这是一个最简单的多用户系统
+// 知识点
+// 1. session
+// 2. 数据库 mysql
+// 3. php语法
+include './mysql.php';
+include './config.php';
+
+$mysql = new MMysql($CONFIG);
+
+
+
+
+
